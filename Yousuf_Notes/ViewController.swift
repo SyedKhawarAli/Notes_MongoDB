1a733311c7040235f03ccacb30e9894b19c442ad
Khawar Ali <khawarali5@gmail.com>
2019-07-21 11:31:34 +0500

    Initial Commit

---
+++ Yousuf_Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yousuf_Notes
+//
+//  Created by khawar on 7/21/19.
+//  Copyright © 2019 khawar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
